8ea716664ef7307aef4bf8e1110ada0d1c8f73af
samaria.anish <samaria.anish@SAMARIAANISH>
2023-02-13 15:04:49 +0530

    Add project files.

---
+++ Swap 2 numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿int number1, number2, tempVariable;
+Console.Write("\nInput the First Number : ");
+number1 = Convert.ToInt32((Console.ReadLine()));
+Console.Write("\nInput the Second Number : ");
+number2 = Convert.ToInt32((Console.ReadLine()));
+tempVariable = number1;
+number1 = number2;
+number2 = tempVariable;
+Console.Write("\nAfter Swapping : ");
+Console.Write("\nFirst Number : " + number1);
+Console.Write("\nSecond Number : " + number2);
+Console.Read();
